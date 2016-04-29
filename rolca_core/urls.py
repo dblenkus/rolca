669b95d2092f67bcc220b5fa106064d6c3df6a63
Domen Blenkuš <domen@blenkus.com>
2016-04-29 18:55:32 +0200

    Rewrite urlpatterns to new format

--- rolca_core/urls.py
+++ rolca_core/urls.py
@@ -1,30 +1,28 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns(  # pylint: disable=invalid-name
-    '',
+from . import views as core_views
 
-    url(r'^$', 'uploader.views.upload_app', name="upload_app"),
+urlpatterns = [  # pylint: disable=invalid-name
+    url(r'^$', core_views.upload_app, name="upload_app"),
     url(r'^potrditev$',
         TemplateView.as_view(template_name='uploader/upload_confirm.html'),
         name="upload_confirm"),
 
-    # url(r'^$', 'uploader.views.upload', name="upload"),
+    # url(r'^seznam$', 'uploader.views.list_select', name="list_select"),
+    # url(r'^seznam/(?P<salon_id>\d+)$', 'uploader.views.list_details',
+    #     name="list_datails"),
 
-    url(r'^seznam$', 'uploader.views.list_select', name="list_select"),
-    url(r'^seznam/(?P<salon_id>\d+)$', 'uploader.views.list_details',
-        name="list_datails"),
+    # url(r'^razpisi$',
+    #     TemplateView.as_view(template_name="uploader/notices.html"),
+    #     name="notices"),
+    # url(r'^razpisi/os$',
+    #     TemplateView.as_view(template_name="uploader/notice_os.html"),
+    #     name="notice_os"),
+    # url(r'^razpisi/ss$',
+    #     TemplateView.as_view(template_name="uploader/notice_ss.html"),
+    #     name="notice_ss"),
 
-    url(r'^razpisi$',
-        TemplateView.as_view(template_name="uploader/notices.html"),
-        name="notices"),
-    url(r'^razpisi/os$',
-        TemplateView.as_view(template_name="uploader/notice_os.html"),
-        name="notice_os"),
-    url(r'^razpisi/ss$',
-        TemplateView.as_view(template_name="uploader/notice_ss.html"),
-        name="notice_ss"),
-
-)
+]
