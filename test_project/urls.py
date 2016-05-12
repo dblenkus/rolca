4d237432d5f020eb41ea7afa39fff5a82981a14f
Domen Blenkuš <domen@blenkus.com>
2016-05-12 22:28:08 +0200

    Add login view to urlpatterns

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    url(r'^accounts/login/$', auth_views.login),
+
     url(r'^', include('rolca_core.urls')),
 ]
