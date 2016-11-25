1b76e90d397ca2ef48dbfc4fe285a3647bd49ac8
Domen Blenkuš <domen@blenkus.com>
2016-11-25 12:35:53 +0100

    Add apps configs

--- rolca/core/__init__.py
+++ rolca/core/__init__.py
@@ -1,1 +1,3 @@
 """Core module for Rolca app."""
+
+default_app_config = 'rolca.core.apps.RolcaCoreConfig'  # pylint: disable=invalid-name
--- rolca/frontend/__init__.py
+++ rolca/frontend/__init__.py
@@ -1,1 +1,3 @@
 """Frontend module for Rolca app."""
+
+default_app_config = 'rolca.frontend.apps.RolcaFrontendConfig'  # pylint: disable=invalid-name
