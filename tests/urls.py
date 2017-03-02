60dab1ead3d37466b38b2890f4c0d9071becab04
Domen Blenkuš <domen@blenkus.com>
2017-03-01 22:30:32 -0600

    Add main project's url config

--- tests/urls.py
+++ tests/urls.py
@@ -3,20 +3,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-from rest_framework import routers
-
-from rolca.core.api import urls as core_api_urls
-
-
-route_lists = [  # pylint: disable=invalid-name
-    core_api_urls.routeList
-]
-
-router = routers.DefaultRouter()  # pylint: disable=invalid-name
-for route_list in route_lists:
-    for prefix, viewset in route_list:
-        router.register(prefix, viewset)
-
 
 urlpatterns = [  # pylint: disable=invalid-name
     url(r'^admin/', include(admin.site.urls)),
@@ -24,8 +10,5 @@ urlpatterns = [  # pylint: disable=invalid-name
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
 
-    url(r'^api/', include(router.urls, namespace='rolca-core-api')),
-
-    url(r'^core/', include('rolca.core.urls', namespace='rolca-core')),
-    url(r'^', include('rolca.frontend.urls', namespace='rolca-frontend')),
+    url(r'^', include('rolca.urls')),
 ]
